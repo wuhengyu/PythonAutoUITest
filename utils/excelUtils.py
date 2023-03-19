df151a13b9e5e2026f9bdc58a5c4350af168f8f2
吴恒宇 <578060214@qq.com>
2023-03-19 15:13:08 +0800

    Initial commit

---
+++ utils/excelUtils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Time    : 2023/3/19 14:46
+# Author  : Walter
+# File    : excelUtils.py
+# License : (C)Copyright Walter
+# Version : 1.0
+# Desc    :
+
+import openpyxl
+
+wb = openpyxl.workbook("../files/openpyxl.xls")
