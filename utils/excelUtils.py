634ebfc95e8395ffd1d721ef7e369b7ab1861af6
吴恒宇 <578060214@qq.com>
2023-03-19 15:30:12 +0800

    add files

--- utils/excelUtils.py
+++ utils/excelUtils.py
@@ -8,4 +8,41 @@
 
 import openpyxl
 
-wb = openpyxl.workbook("../files/openpyxl.xls")
+# class ExcelHandler:
+#     def __init__(self, file_path):
+#         self.file_path = file_path
+#         self.workbook = openpyxl.load_workbook(file_path)
+#         self.sheet = self.workbook.active
+#
+#     def get_cell_value(self, row, column):
+#         return self.sheet.cell(row=row, column=column).value
+#
+#     def set_cell_value(self, row, column, value):
+#         self.sheet.cell(row=row, column=column).value = value
+#
+#     def save_changes(self):
+#         self.workbook.save(self.file_path)
+
+
+class ExcelTool:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.workbook = openpyxl.load_workbook(file_path)
+
+    def get_sheet(self, sheet_name):
+        return self.workbook[sheet_name]
+
+    def get_cell_value(self, sheet_name, row, column):
+        sheet = self.get_sheet(sheet_name)
+        return sheet.cell(row=row, column=column).value
+
+    def set_cell_value(self, sheet_name, row, column, value):
+        sheet = self.get_sheet(sheet_name)
+        sheet.cell(row=row, column=column).value = value
+
+    def save(self):
+        self.workbook.save(self.file_path)
+
+excelHandler = ExcelTool("../files/testExcel.xlsx")
+value = excelHandler.get_cell_value("Sheet1", 1, 1)
+print(value)
